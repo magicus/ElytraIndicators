d43624c055046ec686217bc36dcc4ad68cd3d0f6
Magnus Ihse Bursie <mag@icus.se>
2022-08-24 20:22:00 +0200

    Initial modding framework

---
+++ src/main/java/se/icus/mag/elytraindicators/mixin/ElytraIndicatorsMixin.java
@@ -0,0 +1,26 @@
+package se.icus.mag.elytraindicators.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(InGameHud.class)
+public abstract class ElytraIndicatorsMixin extends DrawableHelper {
+	@Shadow @Final
+	private MinecraftClient client;
+
+	@Inject(method = "render", at = @At("RETURN"))
+	private void render(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+		// FIXME
+	}
+}
